6df8c85581d8208660e6762e4829189f7afad935
ximet <dq.ximet@gmail.com>
2017-03-19 16:42:58 +0300

    initial slider commit: create component with some child element

--- demo/app.js
+++ demo/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import Slider from '../src/Slider.js';
 
 class App extends React.Component {
     constructor(props) {
@@ -8,7 +9,14 @@ class App extends React.Component {
 
     render() {
         return (
-            <div></div>
+            <div style={{width: '70%', margin: 'auto', backgroundColor: '#CCC124'}}>
+                <Slider>
+                    <img src="http://placehold.it/1000x400&text=slide1"/>
+                    <img src="http://placehold.it/1000x400&text=slide2"/>
+                    <img src="http://placehold.it/1000x400&text=slide3"/>
+                    <img src="http://placehold.it/1000x400&text=slide4"/>
+                </Slider>
+            </div>
         )
     }
 }
