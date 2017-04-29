618bb8ffc156a1726769c46283f7a3a117278fd9
ximet <dq.ximet@gmail.com>
2017-04-29 21:26:46 +0300

    add easeInOutQuad

--- src/easingFunctions/easing.js
+++ src/easingFunctions/easing.js
@@ -1,3 +1,13 @@
 export const linear = (time, beginValue, changeInValue, duration) => {
 	return changeInValue * time / duration + beginValue;
 };
+
+export const easeInOutQuad = (time, beginValue, changeInValue, duration) => {
+    if ((time /= duration / 2) < 1) {
+      return changeInValue / 2 * time * time + beginValue;
+    }
+    else {
+      time--;
+      return -changeInValue / 2 * (time * (time - 2) - 1) + beginValue;
+    }
+}
